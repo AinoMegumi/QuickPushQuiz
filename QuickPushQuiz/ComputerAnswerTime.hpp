1e4b9fcfa9ef2a9a43ad4f6cdb08b168f2ef6b76
AinoMegumi <aimegu2014@gmail.com>
2019-04-19 00:33:44 +0900

    add computer answer time generator

---
+++ QuickPushQuiz/ComputerAnswerTime.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <functional>
+#include <random>
+
+class ComputerAnswerTime {
+private:
+	std::uniform_int_distribution<unsigned long long> rand;
+	std::reference_wrapper<std::mt19937> RndEng;
+public:
+	ComputerAnswerTime(const unsigned long long MaxWaitTime, const unsigned long long MinWaitTime, std::mt19937& RandEngine)
+		: rand(MinWaitTime, MaxWaitTime), RndEng(RandEngine) {}
+	unsigned long long ComputerAnswerTime::Generate() const { return this->rand(this->RndEng.get()); }
+};
+
