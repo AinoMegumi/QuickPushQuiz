57796c70ef11aa7b9c1c69426952d9c24eb44399
AinoMegumi <aimegu2014@gmail.com>
2019-04-18 23:55:38 +0900

    add WinMain function base

--- QuickPushQuiz/Main.cpp
+++ QuickPushQuiz/Main.cpp
@@ -1,1 +1,16 @@
-﻿
+﻿#include <Windows.h>
+#include <stdexcept>
+#pragma warning(disable: 6031)
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int CmdShow) {
+	MSG msg{};
+	try {
+		const HRESULT hr = CoInitialize(nullptr);
+		
+	}
+	catch (const std::exception & er) {
+
+	}
+	CoUninitialize();
+	return static_cast<int>(msg.wParam);
+}
