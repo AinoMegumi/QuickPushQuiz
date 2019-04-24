4fc1aaad842ac5a9c6bc91c944ead7bdf070c70e
AinoMegumi <aimegu2014@gmail.com>
2019-04-24 20:57:52 +0900

    change project library DirectX SDK to DxLib

--- QuickPushQuiz/DirectXException.hpp
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include <Windows.h>
-#include <stdexcept>
-
-class DirectXException : public std::exception {
-public:
-	DirectXException() : std::exception() {}
-	DirectXException(const HRESULT hr);
-};
