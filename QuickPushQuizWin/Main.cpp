4e5963a8a1be521b144bbdaa8826788cfc449085
AinoMegumi <aimegu2014@gmail.com>
2019-04-25 23:57:55 +0900

    update directory name

---
+++ QuickPushQuizWin/Main.cpp
@@ -0,0 +1,14 @@
+﻿#include <Windows.h>
+#include <stdexcept>
+
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	try {
+		const HRESULT hr = CoInitialize(nullptr);
+		
+	}
+	catch (const std::exception & er) {
+
+	}
+	CoUninitialize();
+}
+
