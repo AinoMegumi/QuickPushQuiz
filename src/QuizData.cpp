eff9c3736f154bc95fd468246459b0f1810b85e9
AinoMegumi <aimegu2014@gmail.com>
2019-04-25 23:56:58 +0900

    move source file to src directory

--- src/QuizData.cpp
+++ src/QuizData.cpp
@@ -1,5 +1,10 @@
 ﻿#include "QuizData.hpp"
+#ifdef WIN32
 #include "HandleManager.hpp"
+#else
+#include <sys/stat.h>
+#include <dirent.h>
+#endif
 #include <rapidjson/document.h>
 
 QuizData::QuizData(const std::string& FilePath) {
@@ -14,16 +19,35 @@ QuizData::QuizData(const std::string& FilePath) {
 	this->CorrectAnswer = standard::number<unsigned short>(doc["correct"].GetInt(), 0, 4);
 }
 
+#ifdef WIN32
 static inline void ReleaseHandle(HANDLE handle) { FindClose(handle); }
+#endif
+static inline bool isReservedExtensionFile(const std::string& Path, const std::string& Extension) {
+	return Path.substr(Path.find_last_of(".")) == Extension;
+}
 
 std::vector<QuizData> QuizData::LoadQuizData(const std::string& QuizRootDir) {
+#ifdef WIN32
 	WIN32_FIND_DATAA Find{};
 	HandleManager hFind = { FindFirstFileA(QuizRootDir.c_str(), &Find), ReleaseHandle };
 	if (INVALID_HANDLE_VALUE == hFind) return {};
 	std::vector<QuizData> DataArr;
 	do {
 		if (Find.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) continue;
-		DataArr.emplace_back(Find.cFileName);
+		if (isReservedExtensionFile(Find.cFileName, ".json")) DataArr.emplace_back(Find.cFileName);
 	} while (FALSE != FindNextFileA(hFind.Get(), &Find));
+#else
+	dirent** namelist{};
+	const int dirElements = scandir(QuizRootDir.c_str(), &namelist, nullptr, nullptr);
+	if (dirElements == -1) return {};
+	std::vector<QuizData> DataArr{};
+	for (int i = 0; i < dirElements; i++) {
+		struct stat statbuf {};
+		const std::string SearchPath = QuizRootDir + namelist[i]->d_name;
+		if (0 != stat(SearchPath.c_str(), &statbuf)) continue;
+		if (0 == (statbuf.st_mode & S_IFMT)) continue;
+		if (isReservedExtensionFile(SearchPath, ".json")) DataArr.emplace_back(SearchPath);
+	}
+#endif 
 	return DataArr;
 }
