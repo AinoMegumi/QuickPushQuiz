93feb4024622f1e3eff86bba96dd0e5035df5768
AinoMegumi <aimegu2014@gmail.com>
2019-04-27 15:50:03 +0900

    add DxLib init

--- src/DxInit.cpp
+++ src/DxInit.cpp
@@ -1,1 +1,24 @@
-﻿
+﻿#include "DxLib.h"
+#include <stdexcept>
+
+#ifdef __ANDROID__
+void Init() {
+	SetOutApplicationLogValidFlag(FALSE);
+	SetBackgroundColor(255, 255, 255);
+	SetAlwaysRunFlag(TRUE);
+	if (-1 == DxLib_Init()) throw std::runtime_error("Failed to init DxLib");
+	SetTransColor(255, 255, 255);
+	SetDrawScreen(DX_SCREEN_BACK);
+#else
+void Init(const int WindowWidth, const int WindowHeight) {
+	SetMultiThreadFlag(TRUE);
+	SetMainWindowText("もぎ取れ！慰謝料");
+	SetOutApplicationLogValidFlag(FALSE);
+	SetDoubleStartValidFlag(TRUE);
+	SetGraphMode(WindowWidth, WindowHeight, 32);
+	ChangeWindowMode(TRUE);
+	if (-1 == DxLib_Init()) throw std::runtime_error("Failed to init DxLib");
+	SetTransColor(255, 255, 255);
+	SetDrawScreen(DX_SCREEN_BACK);
+#endif
+}
