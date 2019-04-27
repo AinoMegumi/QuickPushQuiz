92257af0af5f4409f25a0f72cb72b18f06a95d55
AinoMegumi <aimegu2014@gmail.com>
2019-04-27 21:27:02 +0900

    add display info get function

--- src/Core.cpp
+++ src/Core.cpp
@@ -1,8 +1,23 @@
 ﻿#include "Core.hpp"
+#ifdef WIN32
+#include "CppIniRead.hpp"
+#else
+#include "DxLib.h"
+#endif
 
 namespace Core {
 	int DisplayWidth;
 	int DisplayHeight;
+	void LoadScreenSizeInfo() {
+#ifdef WIN32
+		const IniReadA ini(".\\System\\config.ini");
+		DisplayWidth = ini.GetNum<int>("Display", "Width", 800);
+		DisplayHeight = ini.GetNum<int>("Display", "Height", 600);
+#else
+		int ColorBit = 0;
+		GetScreenState(&DisplayWidth, &DisplayHeight, &ColorBit);
+#endif
+	}
 #ifdef WIN32
 	HINSTANCE InsanceHandle;
 #endif
