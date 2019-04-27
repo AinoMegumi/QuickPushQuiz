ec427d84bc289360567da9606fe4b18b6d89fb49
AinoMegumi <aimegu2014@gmail.com>
2019-04-27 21:32:45 +0900

    add DxLib initialize function and process when exception occured

--- QuickPushQuizWin/Main.cpp
+++ QuickPushQuizWin/Main.cpp
@@ -1,13 +1,14 @@
 ﻿#include "Core.hpp"
+#include "DxInit.hpp"
 #include <stdexcept>
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	Core::InsanceHandle = hInstance;
 	try {
-		
+		InitDxLib(Core::DisplayWidth, Core::DisplayHeight);
 	}
 	catch (const std::exception & er) {
-
+		Core::MessageBox(er.what(), "エラー", MB_ICONERROR | MB_OK);
 	}
 	CoUninitialize();
 }
