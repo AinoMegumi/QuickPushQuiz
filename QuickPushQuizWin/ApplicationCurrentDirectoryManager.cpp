9f66bf082a8c0d073a0fa445239a2f4595cd492d
AinoMegumi <aimegu2014@gmail.com>
2019-05-02 21:39:27 +0900

    add application current directory manager to Windows project

---
+++ QuickPushQuizWin/ApplicationCurrentDirectoryManager.cpp
@@ -0,0 +1,34 @@
+﻿#include "ApplicationCurrentDirectoryManager.hpp"
+#include "Win32Exception.hpp"
+
+static inline std::string GetModuleFilePath() {
+	std::string Buffer;
+	Buffer.resize(MAX_PATH);
+	if (0 == GetModuleFileNameA(NULL, &Buffer[0], MAX_PATH)) throw Win32Exception();
+	Buffer.resize(std::strlen(Buffer.c_str()));
+	return Buffer.substr(0, Buffer.find_last_of("\\") + 1);
+}
+
+static inline std::string GetApplicationCurrentDirectory() {
+	std::string Buffer;
+	Buffer.resize(MAX_PATH);
+	if (0 == GetCurrentDirectoryA(MAX_PATH, &Buffer[0])) throw Win32Exception();
+	Buffer.resize(std::strlen(Buffer.c_str()));
+	return Buffer;
+}
+
+ApplicationCurrentDirectoryManager::ApplicationCurrentDirectoryManager(const bool UseModuleFilePath) 
+	: CurrentDirectory(UseModuleFilePath ? GetModuleFilePath() : GetApplicationCurrentDirectory()){}
+
+void ApplicationCurrentDirectoryManager::ChangeAppCurrentDirectory(const bool UseModuleFilePath) {
+	this->CurrentDirectory = UseModuleFilePath ? GetModuleFilePath() : GetApplicationCurrentDirectory();
+}
+
+void ApplicationCurrentDirectoryManager::ChangeAppCurrentDirectory(const std::string& NewDirectory) {
+	this->CurrentDirectory = NewDirectory;
+}
+
+std::string ApplicationCurrentDirectoryManager::ChangeFullPath(const std::string& RelativePath) const {
+	return RelativePath.at(1) == ':' ? RelativePath : (this->CurrentDirectory + "\\" + RelativePath);
+}
+
---
+++ QuickPushQuizWin/ApplicationCurrentDirectoryManager.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <string>
+
+class ApplicationCurrentDirectoryManager {
+private:
+	std::string CurrentDirectory;
+public:
+	ApplicationCurrentDirectoryManager(const bool UseModuleFilePath = false);
+	// カレントディレクトリをモジュールパスに変更する
+	void ChangeAppCurrentDirectory(const bool UseModuleFilePath = false);
+	// カレントディレクトリを指定パスにする
+	void ChangeAppCurrentDirectory(const std::string& NewDirectory);
+	// 相対パスを絶対パスに変更する
+	std::string ChangeFullPath(const std::string& RelativePath) const;
+};
