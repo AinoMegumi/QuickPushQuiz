bec452ac2975b82bb5c506e97b868b74bb7d19ac
AinoMegumi <aimegu2014@gmail.com>
2019-05-03 23:36:05 +0900

    add operator converting between relative coordinate to absolute coordinate

---
+++ src/Coordinate.cpp
@@ -0,0 +1,10 @@
+﻿#include "Coordinate.hpp"
+#include "Core.hpp"
+
+Coordinate Coordinate::operator<<(const Position& p) {
+	return { static_cast<int>(Core::DisplayWidth * p.x + 0.5), static_cast<int>(Core::DisplayHeight * p.y + 0.5) };
+}
+
+Position Position::operator<<(const Coordinate& c) {
+	return { static_cast<float>(c.x) / Core::DisplayWidth, static_cast<float>(c.y) / Core::DisplayHeight };
+}
--- src/Coordinate.hpp
+++ src/Coordinate.hpp
@@ -1,5 +1,11 @@
 ﻿#pragma once
+struct Position;
+
+// 絶対座標系
 struct Coordinate {
+	Coordinate() = default;
+	constexpr Coordinate(const int X, const int Y) : x(X), y(Y) {}
+	Coordinate operator << (const Position& p);
 	int x;
 	int y;
 };
