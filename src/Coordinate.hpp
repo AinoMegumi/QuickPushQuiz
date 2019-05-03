ecb236666ec4fe1f72b9cd2f251236e68bef3a77
AinoMegumi <aimegu2014@gmail.com>
2019-05-03 23:34:32 +0900

    add relative coordinate structure

--- src/Coordinate.hpp
+++ src/Coordinate.hpp
@@ -1,5 +1,13 @@
-#pragma once
+﻿#pragma once
 struct Coordinate {
 	int x;
 	int y;
+};
+
+// 相対座標系
+struct Position {
+	Position() = default;
+	constexpr Position(const float X, const float Y) : x(X), y(Y) {}
+	Position operator << (const Coordinate& c);
+	float x, y;
 };
