4411e4d15fb83dcc84ea8ef5abd32a5ea3b1c6e1
AinoMegumi <aimegu2014@gmail.com>
2019-05-05 12:35:05 +0900

    add MainProcess in WinMain function

--- QuickPushQuizWin/Main.cpp
+++ QuickPushQuizWin/Main.cpp
@@ -1,15 +1,17 @@
-﻿#include "Core.hpp"
-#include "DxInit.hpp"
+﻿#include "MainProcess.hpp"
+#include "Core.hpp"
 #include <stdexcept>
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	Core::InsanceHandle = hInstance;
 	try {
-		InitDxLib(Core::DisplayWidth, Core::DisplayHeight);
+		MainProcess main{};
+		main.run();
 	}
 	catch (const std::exception & er) {
 		Core::MessageBox(er.what(), "エラー", MB_ICONERROR | MB_OK);
 	}
 	CoUninitialize();
+	return 0;
 }
 
