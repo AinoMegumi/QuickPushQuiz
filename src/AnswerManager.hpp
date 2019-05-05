c0be69ed9fadb0f86290379fd759208ab8224993
AinoMegumi <aimegu2014@gmail.com>
2019-05-05 12:36:12 +0900

    add answer management class

---
+++ src/AnswerManager.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+class AnswerManager {
+private:
+	bool PlayerAnswered;
+	bool EnemyAnswered;
+public:
+	AnswerManager() : PlayerAnswered(false), EnemyAnswered(false) {}
+	void Reset() {
+		this->PlayerAnswered = false;
+		this->EnemyAnswered = false;
+	}
+	void SetPlayerAnswered() { this->PlayerAnswered = true; }
+	void SetEnemyAnswered() { this->EnemyAnswered = true; }
+	bool loop() const noexcept { return !this->PlayerAnswered || !this->EnemyAnswered; }
+};
