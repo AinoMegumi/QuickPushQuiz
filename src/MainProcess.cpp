a0aa95832144323b6844e22a54c8b77b5a7d85bd
AinoMegumi <aimegu2014@gmail.com>
2019-05-05 12:36:00 +0900

    add game main process

---
+++ src/MainProcess.cpp
@@ -0,0 +1,33 @@
+﻿#include "MainProcess.hpp"
+#include "AnswerManager.hpp"
+#include "Core.hpp"
+#include "DxInit.hpp"
+
+MainProcess::MainProcess() : Graph() {
+#ifndef WIN32
+	InitDxLib();
+	Core::LoadScreenSizeInfo();
+	this->NativeActivity = DxLib::GetNativeActivity();
+	if (JNI_OK != this->NativeActivity->vm->AttachCurrentThreadAsDaemon(&this->env, nullptr))
+		throw std::runtime_error("Failed to associate Java VM with thread of software execution");
+#else
+	Core::LoadScreenSizeInfo();
+	InitDxLib(Core::DisplayWidth, Core::DisplayHeight);
+#endif
+}
+
+MainProcess::~MainProcess() { 
+#ifndef WIN32
+	this->NativeActivity->vm->DetachCurrentThread();
+#endif
+	DxLib_End();
+}
+
+void MainProcess::run() {
+
+}
+
+void MainProcess::GameMain() {
+	AnswerManager answer{};
+
+}
---
+++ src/MainProcess.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "DxInit.hpp"
+#include "DxGraphic.hpp"
+#include "DxLib.h"
+
+class MainProcess {
+private:
+	GraphicManager Graph;
+#ifdef __ANDROID__
+	JNIEnv* env;
+	const ANativeActivity* NativeActivity;
+#endif
+public:
+	MainProcess();
+	~MainProcess();
+	MainProcess(const MainProcess&) = delete;
+	MainProcess(MainProcess&&) = delete;
+	MainProcess& operator = (const MainProcess&) = delete;
+	MainProcess& operator = (MainProcess&&) = delete;
+	void run();
+	void GameMain();
+};
