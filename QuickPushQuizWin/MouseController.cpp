7fc858d898001b2ac31e9c1e4778c4dc548430d9
AinoMegumi <aimegu2014@gmail.com>
2019-05-06 23:49:46 +0900

    start creating mouse control

---
+++ QuickPushQuizWin/MouseController.cpp
@@ -0,0 +1,12 @@
+﻿#include "MouseController.hpp"
+#include "DxLib.h"
+
+void MouseController::UpdateCurrentPos() {
+	GetMousePoint(&this->Current.x, &this->Current.y);
+}
+
+bool MouseController::Clicked() {
+	return Core::LeftHandMouse
+		? GetMouseInput() & MOUSE_INPUT_RIGHT
+		: GetMouseInput() & MOUSE_INPUT_LEFT;
+}
---
+++ QuickPushQuizWin/MouseController.hpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "GameController.hpp"
+
+class MouseController : public GameController {
+public:
+	MouseController() : GameController() {}
+	void UpdateCurrentPos() override;
+	bool Clicked() override;
+};
