6ca8b7418e576d2e51e59b9c28052a5bff8c63fb
AinoMegumi <aimegu2014@gmail.com>
2019-05-06 15:28:53 +0900

    refactor and fix build error on coordinate management class in Coordinate.hpp

--- src/Coordinate.hpp
+++ src/Coordinate.hpp
@@ -1,27 +1,32 @@
 ﻿#pragma once
 #include "Core.hpp"
 
-// 絶対座標系
-struct Coordinate {
-	Coordinate() = default;
-	constexpr Coordinate(const int X, const int Y) : x(X), y(Y) {}	
-	int x;
-	int y;
-};
+namespace Coordinate {
+	// 絶対座標系
+	struct Absolute {
+		Absolute() = default;
+		constexpr Absolute(const int X, const int Y) : x(X), y(Y) {}
+		int x;
+		int y;
+	};
 
-// 相対座標系
-struct Position {
-	Position() = default;
-	constexpr Position(const float X, const float Y) : x(X), y(Y) {}
-	Coordinate ToCoordinate() const;
-	Position operator << (const Coordinate& c);
-	float x, y;
-};
+	// 相対座標系
+	struct Relative {
+		Relative() = default;
+		constexpr Relative(const float X, const float Y) : x(X), y(Y) {}
+		float x, y;
+	};
 
-void operator << (Coordinate &c, const Position p) {
-	c = { static_cast<int>(static_cast<float>(Core::DisplayWidth) * p.x + 0.5), static_cast<int>(static_cast<float>(Core::DisplayHeight) * p.y + 0.5) };
-}
-
-void operator << (Position &p, const Coordinate & c) {
-	p = { static_cast<float>(c.x) / static_cast<float>(Core::DisplayWidth), static_cast<float>(c.y) / static_cast<float>(Core::DisplayHeight) };
+	inline Absolute to(const Relative& r) { 
+		return Absolute(
+			static_cast<int>(static_cast<float>(Core::DisplayWidth) * r.x + 0.5),
+			static_cast<int>(static_cast<float>(Core::DisplayHeight) * r.y + 0.5)
+		);
+	}
+	inline Relative to(const Absolute& a) {
+		return Relative(
+			static_cast<float>(a.x) / static_cast<float>(Core::DisplayWidth),
+			static_cast<float>(a.y) / static_cast<float>(Core::DisplayHeight)
+		);
+	}
 }
